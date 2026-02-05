672831cdc61ea0851e7747dc886d4acabb915f85
Bell <natthakitrodruean@gmail.com>
2026-02-05 11:52:38 +0700

    add : try - catch

--- CalculateProject/Main.java
+++ CalculateProject/Main.java
@@ -3,9 +3,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+    try {
         System.out.println("Add Hot fix1");
         System.out.println("Add Hot fix2");
         System.out.println("Add Hot fix3");
         System.out.println("Add Hot fix4");
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+        
     }
 }
