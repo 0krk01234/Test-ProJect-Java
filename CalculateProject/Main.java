9454ae4f801f807e6ef0035b13f624294c3ca126
0krk01234 <ruizfeez99@gmail.com>
2026-02-05 12:03:46 +0700

    Add update code

--- CalculateProject/Main.java
+++ CalculateProject/Main.java
@@ -1,11 +1,17 @@
 package CalculateProject;
 import java.util.Scanner;
 
+import javax.print.attribute.standard.DialogOwner;
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("add add");
         System.out.println("Add Hot fix1");
         System.out.println("Add Hot fix2");
         System.out.println("Add Hot fix3");
         System.out.println("Add Hot fix4");
+
+        Display D1 = new Display();
     }
 }
