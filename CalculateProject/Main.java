2cc63d17584fb8b95007b30b8aee2952392d5cbe
0krk01234 <ruizfeez99@gmail.com>
2026-02-05 11:59:00 +0700

    Update Code

--- CalculateProject/Main.java
+++ CalculateProject/Main.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+    try {
         System.out.println("Add Hot fix1");
         System.out.println("Add Hot fix2");
         System.out.println("Add Hot fix3");
         System.out.println("Add Hot fix4");
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
+
+    Display D1 = new Display();
+    System.out.println("eiei");
+    System.err.println("eeeee");
+        
     }
 }
